a08286ee1428eaf3b323691734f6420c44eed715
matheuskassagas <gigliotti_@hotmail.com>
2022-07-20 10:41:41 -0300

    subnets created

--- vpc.tf
+++ vpc.tf
@@ -5,5 +5,33 @@ resource "aws_vpc" "this" {
 
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
-  tags = merge(local.common_tags, {Name = "Terraform VPC"})
+  tags = merge(local.common_tags, {Name = "Terraform IGW"})
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id = aws_vpc.this
+  cidr_block = "192.168.0.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = merge(local.common_tags, {Name = "Public A"})
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id = aws_vpc.this
+  cidr_block = "192.168.1.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = merge(local.common_tags, {Name = "Public B"})
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id = aws_vpc.this
+  cidr_block = "192.168.2.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags = merge(local.common_tags, {Name = "Private A"})
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id = aws_vpc.this
+  cidr_block = "192.168.3.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags = merge(local.common_tags, {Name = "Private B"})
 }
